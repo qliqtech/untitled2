54e6a9cfb743db2a43b821a03661a91eb7c9ea12
David Kumi <davidkumi10@gmail.com>
2020-12-15 13:03:42 +0000

    Changes made to route

--- blog/app/Http/Middleware/AdminAuth.php
+++ blog/app/Http/Middleware/AdminAuth.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
 
 class AdminAuth
 {
@@ -15,6 +16,11 @@ class AdminAuth
      */
     public function handle($request, Closure $next)
     {
-        return $next($request);
+        if (Auth::guard('api')->check() && $request->user()->type >= 1) {
+            return $next($request);
+        } else {
+            $message = ["message" => "Permission Denied"];
+            return response($message, 401);
+        }
     }
 }
